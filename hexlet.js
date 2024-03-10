87bdbadffb6db78f69194dff25be40d5fda21e83
DashaIgnatieva <dariyignatieva@mail.ru>
2024-03-10 14:07:59 +0300

    Исправленный вариант программы

--- hexlet.js
+++ hexlet.js
@@ -1,17 +1,31 @@
+// const playRound = () => {
+//     const makeNumber = () => Math.round(Math.random() * 100);
+
+//     const isEven = (number) => number % 2 === 0;
+
+//     const number = makeNumber();
+
+//     const userAnswer = prompt(`Является ли чило ${number} четным? Ответьте "да" или "нет".`);
+//     const correctAnswer = isEven(number) ? 'да' : 'нет';
+
+//     console.log(number, correctAnswer, userAnswer)
+
+//     return userAnswer && userAnswer.toLowerCase().trim() === correctAnswer;
+// };
+
 const playRound = () => {
     const makeNumber = () => Math.round(Math.random() * 100);
 
-    const isEven = (number) => number % 2 === 0;
+    const numberOne = makeNumber();
+    const numberTwo = makeNumber();
 
-    const number = makeNumber();
+    const userAnswer = prompt(`Какова сумма числа ${numberOne} и числа ${numberTwo}?`);
+    const correctAnswer = numberOne + numberTwo
 
-    const userAnswer = prompt(`Является ли чило ${number} четным? Ответьте "да" или "нет".`);
-    const correctAnswer = isEven(number) ? 'да' : 'нет';
+    console.log(`Числа: ${numberOne}, ${numberTwo}\nПравильный ответ: ${correctAnswer}\nОтвет пользователя: ${userAnswer}`)
 
-    console.log(number, correctAnswer, userAnswer)
-
-    return userAnswer && userAnswer.toLowerCase().trim() === correctAnswer;
-};
+    return userAnswer && userAnswer.toLowerCase().trim() === correctAnswer.toString();
+}
 
 const playGame = () => {
     for (let i = 0; i < 3; i++) {
@@ -29,5 +43,3 @@ const playGame = () => {
 
 const button = document.querySelector('button'); //создали сам обьект кнопки
 button.addEventListener('click', playGame);  // слушаем кнопку и при клике включаем игру
-
-''.length
